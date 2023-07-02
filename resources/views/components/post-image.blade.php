b7cc379066373f6b6810aef4ad9d28c252a59a8c
MOohamedMahfouz <mouhamadmahfouz20@gmail.com>
2023-07-02 13:51:57 +0300

    add asset() to thumbnail path

--- resources/views/components/post-image.blade.php
+++ resources/views/components/post-image.blade.php
@@ -2,5 +2,5 @@
 
 
 <div class="fill">
-    <img src="/storage/{{$thumbnail}}" alt="" width="1000" height="500">
+    <img src="{{asset("/storage/$thumbnail")}}" alt="" width="1000" height="500">
 </div>
